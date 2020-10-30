e5f9d156aadd07a089c0f7d33993299ebb3bb443
Thoms-C <thomas.clment@gmail.com>
2020-10-30 14:49:57 +0100

    exo8 react

--- src/App.js
+++ src/App.js
@@ -4,34 +4,24 @@ import DisplaySimpson from "./DisplaySimpson.jsx";
 
 import "./App.css";
 
-const sampleSimpson = 
-[
-  {
-   "quote": "Shoplifting is a victimless crime, like punching someone in the dark.",
-   "character": "Nelson Muntz",
-   "image" : "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
-   "characterDirection" : "Left"
-  }
-]
-;
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      simpson: sampleSimpson[0],
+      simpson: null,
     };
     this.getSimpson = this.getSimpson.bind(this);
   }
   
+  componentDidMount(){
+    this.getSimpson()
+  }
+
+
   getSimpson() {
-    // Send the request
     axios
       .get("https://thesimpsonsquoteapi.glitch.me/quotes")
-      // Extract the DATA from the received response
-
       .then((response) => response.data)
-      // Use this data to update the state
       .then((data) => {
         console.log(data);
         this.setState({
@@ -45,7 +35,8 @@ class App extends Component {
       <div className="App">
          <h1>Simpsons</h1>
          
-        <DisplaySimpson simpson={this.state.simpson} />
+         {this.state.simpson ? <DisplaySimpson simpson={this.state.simpson}/> : <p>No data</p>}
+         
         <button type="button" onClick={this.getSimpson}>
           Get quotes
         </button>
